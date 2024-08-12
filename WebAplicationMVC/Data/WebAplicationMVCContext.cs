af95ac18561929f33def378c1b7addf909e60ae4
Mateus <mateus.f.2804@gmail.com>
2024-08-12 15:27:43 -0300

    Mandando

---
+++ WebAplicationMVC/Data/WebAplicationMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAplicationMVC.Models;
+
+namespace WebAplicationMVC.Data
+{
+    public class WebAplicationMVCContext : DbContext
+    {
+        public WebAplicationMVCContext (DbContextOptions<WebAplicationMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAplicationMVC.Models.Department> Department { get; set; } = default!;
+    }
+}
